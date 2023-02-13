1b840cc04254ae3a84d349484691872d81a08765
Dany serfaty <davidserfaty2315@gmail.com>
2023-02-13 17:00:01 +0200

    fait le carousel

--- client/src/components/Carroussel.js
+++ client/src/components/Carroussel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@mui/material'
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import photo1 from '../assets/photo1.jpg'
 import photo2 from '../assets/photo2.jpg'
 import "./Carroussel.css"
@@ -21,7 +22,28 @@ function CarouselFunc(props)
     ]
 
     return (
-        <Carousel>
+        <Carousel
+        IndicatorIcon={<RadioButtonUncheckedIcon fontSize='small'/>} // Previous Example
+        indicatorIconButtonProps={{
+            style: {
+                padding: '10px',    // 1
+                color: 'black'       // 3
+            }
+        }}
+        activeIndicatorIconButtonProps={{
+            style: {
+                backgroundColor: 'rgb(184, 182, 182)' // 2
+            }
+        }}
+        indicatorContainerProps={{
+            style: {
+                marginTop: '10px', // 5
+                textAlign: 'center' // 4
+            }
+    
+        }} >
+
+
             {
                 items.map( (item, i) =>  <Item key={i} item={item} /> )
             }
@@ -37,7 +59,7 @@ function Item(props)
     return (
 
     
-        <Paper className='paperStyle' elevation={4} style={ backgroundImage: `url(${props.item.src})`}>
+        <Paper className='paperStyle' elevation={4} style={{backgroundImage: `url(${props.item.src})`}}>
             
                 <h2 className="a">{props.item.name}</h2>
                 <p className="a">{props.item.description}</p>

cf2e9bfbcc3bff022fb87bee7cdbad757bc9de92
Dany serfaty <davidserfaty2315@gmail.com>
2023-03-06 23:46:48 +0200

    reussi le responsiv la il manque le  burger menu pour mobile

--- client/src/layout/NavBar/NavBar.js
+++ client/src/layout/NavBar/NavBar.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router";
 import './NavBar.css'
 import SearchNavBar from "../../components/SearchNavBar/SearchNavBar.js";
 import { Link } from "react-router-dom";
 import { Avatar } from "@mui/material";
 
+import { Divide  } from 'hamburger-react'
+
 function NavBar (){
+    const width = window.innerWidth
+
     const connect = false
+
+useEffect(()=>{
+    if (width<=1100) {
+        console.log(width)
+    }
+})
 
 
     return(
 <>
         <div className="navBar">
-            <div style={{marginTop:"1ch", color:"#007193"}}>LOGO</div>
+            <div className="logo" style={{marginTop:"1ch", color:"#007193"}}>LOGO</div>
 
 
             <div className="menu">
@@ -40,7 +50,11 @@ function NavBar (){
                      :  <div className="login">
                             <Link to="login" className="seConnecter">Se connecter</Link>
                         </div>
-            }                   
+            }     
+            <button className="burger"  >
+                
+               <Divide size={20} duration={0.8} color='#007193' ></Divide>
+            </button>              
 
                 
         </div>
